c9c12ccd7ff022ea9ba437ecd11551741f8b257e
pavlobornia <pavlo.bornia@essenceglobal.com>
2019-08-06 16:49:37 +0300

    entrypoint file

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,10 +3,4 @@
 set -e
 source /assets/colorecho
 
-if [ ! -d "/opt/oracle/app/product/11.2.0/dbhome_1" ]; then
-	echo_yellow "Database is not installed. Installing..."
-	/assets/install.sh
-fi
-
 su oracle -c "/assets/entrypoint_oracle.sh"
-
--- assets/entrypoint_oracle.sh
+++ assets/entrypoint_oracle.sh
@@ -35,27 +35,8 @@ start_db() {
 		exit 0
 	EOF
 	while read line; do echo -e "sqlplus: $line"; done
-	wait $MON_ALERT_PID
 }
 
-create_db() {
-	echo_yellow "Database does not exist. Creating database..."
-	date "+%F %T"
-	monitor $alert_log alertlog &
-	MON_ALERT_PID=$!
-	monitor $listener_log listener &
-	#lsnrctl start | while read line; do echo -e "lsnrctl: $line"; done
-	#MON_LSNR_PID=$!
-        echo "START DBCA"
-	dbca -silent -createDatabase -responseFile /assets/dbca.rsp
-	echo_green "Database created."
-	date "+%F %T"
-	change_dpdump_dir
-        touch $pfile
-	trap_db
-        kill $MON_ALERT_PID
-	#wait $MON_ALERT_PID
-}
 
 stop() {
     trap '' SIGINT SIGTERM
@@ -77,21 +58,12 @@ shu_immediate() {
 	while read line; do echo -e "sqlplus: $line"; done
 }
 
-change_dpdump_dir () {
-	echo_green "Changind dpdump dir to /opt/oracle/dpdump"
-	sqlplus / as sysdba <<-EOF |
-		create or replace directory data_pump_dir as '/opt/oracle/dpdump';
-		commit;
-		exit 0
-	EOF
-	while read line; do echo -e "sqlplus: $line"; done
-}
-
-chmod 777 /opt/oracle/dpdump
-
 echo "Checking shared memory..."
 df -h | grep "Mounted on" && df -h | egrep --color "^.*/dev/shm" || echo "Shared memory is not mounted."
-if [ ! -f $pfile ]; then
-  create_db;
-fi 
+
 start_db
+
+### Run User Defined Scripts
+bash /assets/runUserScripts.sh
+
+wait $MON_ALERT_PID
